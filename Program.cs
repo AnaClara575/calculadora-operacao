afbb455737186a3c121daa35eda3000263ea56cf
Ana Clara ♡ <ana.c.santos575@aluno.senai.br>
2026-05-07 14:13:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Digite o primeiro numero: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int resultado = numero1 + numero2;
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
